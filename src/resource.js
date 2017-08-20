db137a0eb4a11fe1c8750358524c9ff1370fcadd
mtti <mtti@users.noreply.github.com>
2017-08-20 16:12:10 +0300

    Add listings and subresources

--- src/resource.js
+++ src/resource.js
@@ -1,4 +1,5 @@
 
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { NotFoundError, AuthorizationError } = require('./errors');
@@ -21,19 +22,39 @@ class Resource {
       this.authorizationCallback = () => true;
     }
 
+    this.operations = options.operations || ['list', 'create', 'read', 'update', 'delete'];
+    this.listAttributes = options.listAttributes || ['id'];
+    this.listOrder = options.listOrder || [['createdAt', 'ASC']];
+    this.listConditionsCallback = options.listConditionsCallback || function() { return {}; };
+    this.parentPropertyName = options.parentPropertyName || null;
+    this.parentForeignKey = options.parentForeignKey || null;
+    this.filterCallback = options.filterCallback || function(item) { return item; };
+
     this.requireInstance = this.requireInstance.bind(this);
     this.requireAuthorization = this.requireAuthorization.bind(this);
+    this.list = this.list.bind(this);
     this.create = this.create.bind(this);
     this.read = this.read.bind(this);
     this.update = this.update.bind(this);
     this.delete = this.update.bind(this);
 
-    this.router.post('/', jsonParser, this.requireAuthorization('create'), this.create);
-    this.router.get('/:id', this.requireInstance, this.requireAuthorization('read'), this.read);
-    this.router.put('/:id', this.requireInstance, jsonParser, this.requireAuthorization('update'),
-      this.update);
-    this.router.delete('/:id', this.requireInstance, this.requireAuthorization('delete'),
-      this.delete);
+    if (_.includes(this.operations, 'list')) {
+      this.router.get('/', this.requireAuthorization('list'), this.list);
+    }
+    if (_.includes(this.operations, 'create')) {
+      this.router.post('/', jsonParser, this.requireAuthorization('create'), this.create);
+    }
+    if (_.includes(this.operations, 'read')) {
+      this.router.get('/:id', this.requireInstance, this.requireAuthorization('read'), this.read);
+    }
+    if (_.includes(this.operations, 'update')) {
+      this.router.put('/:id', this.requireInstance, jsonParser, this.requireAuthorization('update'),
+        this.update);
+    }
+    if (_.includes(this.operations, 'delete')) {
+      this.router.delete('/:id', this.requireInstance, this.requireAuthorization('delete'),
+        this.delete);
+    }
   }
 
   requireInstance(req, res, next) {
@@ -70,6 +91,19 @@ class Resource {
     };
   }
 
+  subResource(slug, model, foreignKey, options={}) {
+    const optionsCopy = _.cloneDeep(options);
+    optionsCopy.parentForeignKey = foreignKey;
+    optionsCopy.parentPropertyName = this.propertyName;
+
+    const subResource = new Resource(model, optionsCopy);
+
+    this.router.use(`/:id/${slug}`, this.requireInstance, this.requireAuthorization('subResource'),
+      subResource.router);
+
+    return subResource;
+  }
+
   operation(operationName, handler) {
     this.router.post(`/${operationName}`, jsonParser, this.requireAuthorization(operationName),
       handler);
@@ -80,12 +114,52 @@ class Resource {
       this.requireAuthorization(operationName), handler);
   }
 
+  filterOne(item, operation, req) {
+    if (!item) {
+      return null;
+    }
+    return this.filterCallback(item, operation, req);
+  }
+
+  filterMany(items, operation, req) {
+    if (!items) {
+      return [];
+    }
+    return items
+      .map(item => this.filterOne(item, operation, req))
+      .filter(item => item);
+  }
+
+  list(req, res, next) {
+    const query = {
+      attributes: this.listAttributes,
+      where: this.listConditionsCallback(req),
+      order: this.listOrder,
+    };
+
+    if (this.parentForeignKey && this.parentPropertyName) {
+      query.where[this.parentForeignKey] = req[this.parentPropertyName].id;
+    }
+
+    this.model.findAll(query)
+      .then((rows) => {
+        res.json(this.filterMany(rows, 'list', req));
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+
   create(req, res, next) {
-    delete req.body.id;
-    const instance = this.model.build(req.body);
-    instance.save()
+    const body = _.cloneDeep(req.body);
+    delete body.id;
+    if (this.parentForeignKey && this.parentPropertyName) {
+      body[this.parentForeignKey] = req[this.parentPropertyName].id;
+    }
+
+    this.model.create(body)
       .then(() => {
-        res.json(instance);
+        res.json(this.filterOne(instance, 'create', req));
       })
       .catch((err) => {
         next(err);
@@ -93,7 +167,8 @@ class Resource {
   }
 
   read(req, res) {
-    res.json(req[this.propertyName]);
+    const instance = this.filterOne(req[this.propertyName], 'read', 'req');
+    res.json(instance);
   }
 
   update(req, res, next) {
@@ -103,7 +178,7 @@ class Resource {
 
     req[this.propertyName].update(req.body)
       .then((instance) => {
-        res.json(instance);
+        res.json(this.filterOne(instace, 'update', req));
       })
       .catch((err) => {
         next(err);
